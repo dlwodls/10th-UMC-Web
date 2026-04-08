ecc49f539c7f2cf0730d496c875664461e5fd472
jilee <jilee0818@gmail.com>
2026-04-09 05:32:50 +0900

    로그인 기능 구현
    - useForm 커스텀 훅
    
    - 유효성 검증
    
    - 서버 연동

--- UMC-10th-mission-FE/src/App.tsx
+++ UMC-10th-mission-FE/src/App.tsx
@@ -1,10 +1,31 @@
 import './App.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import HomePage from "./pages/HomePage.tsx";
+import NotFoundPage from "./pages/NotFoundPage.tsx";
+import LoginPage from "./pages/LoginPage.tsx";
+import HomeLayout from './layouts/HomeLayout.tsx';
+import SignupPage from './pages/SignupPage.tsx';
+
+// 1. 홈페이지
+// 2. 로그인 페이지
+// 3. 회원가입 페이지
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <HomeLayout />,
+    errorElement: <NotFoundPage />,
+    children: [
+      { index: true, element: <HomePage /> },
+    ],
+  },
+  { path: "/login", element: <LoginPage /> },
+  { path: "/signup", element: <SignupPage /> },
+]);
 
 function App() {
 
-  return (
-    <></>
-  )
+  return <RouterProvider router={router} />;
 }
 
-export default App
+export default App;
---
+++ UMC-10th-mission-FE/src/pages/SignupPage.tsx
@@ -0,0 +1,7 @@
+const SignupPage = () => {
+    return (
+        <div>Home Page</div>
+    );
+};
+
+export default SignupPage;
